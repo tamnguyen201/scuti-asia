2042e7e1875a43010feb250edf496edaeb6169bf
Tam Nguyen <tam2012000@gmail.com>
2020-08-06 18:26:08 +0700

    code branch master

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Role;
+
+class RoleController extends Controller
+{
+    
+    public function index()
+    {
+        $roles = Role::all();
+        return view("admin.role.index", compact('roles'));
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
