ddc0bc9469f1c99ea348ece2d61a07e03f9c1e30
linhnn1625 <linhnn160295@gmail.com>
2020-08-14 09:17:52 +0700

    sửa viết tắt

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -11,22 +11,22 @@ use App\Repositories\Locations\LocationRepositoryInterface;
 
 class LocationController extends Controller
 {
-    protected $locationRepo;
+    protected $locationRepository;
 
-    public function __construct(LocationRepositoryInterface $locationRepo)
+    public function __construct(LocationRepositoryInterface $locationRepository)
     {
-        $this->locationRepo = $locationRepo;
+        $this->locationRepository = $locationRepository;
     }
 
     public function index()
     {
-        $locations = $this->locationRepo->paginate(10);
+        $locations = $this->locationRepository->paginate(10);
         return view("admin.location.index", compact('locations'));
     }
 
     public function edit($id)
     {
-        $location = $this->locationRepo->show($id);
+        $location = $this->locationRepository->show($id);
         $html = view('admin.location.edit', compact('location'))->render();
         return response()->json($html);
     }
@@ -39,23 +39,23 @@ class LocationController extends Controller
 
     public function store(LocationRequest $request)
     {
-        $this->locationRepo->create($request->all());
-        $locations = $this->locationRepo->paginate(10);
+        $this->locationRepository->create($request->all());
+        $locations = $this->locationRepository->paginate(10);
         $html = view('admin.location.list', compact('locations'))->render();
         return response()->json($html);
     }
 
     public function update(LocationUpdateRequest $request)
     {
-        $this->locationRepo->update($request->all(), $request->id);
-        $locations = $this->locationRepo->paginate(10);
+        $this->locationRepository->update($request->all(), $request->id);
+        $locations = $this->locationRepository->paginate(10);
         $html = view('admin.location.list', compact('locations'))->render();
         return response()->json($html);
     }
 
     public function destroy($id)
     {
-        $this->locationRepo->delete($id);
+        $this->locationRepository->delete($id);
         return redirect()->back()->with('success', config('common.alert_messages.success'));
     }
 }
