1c10bde3958454159e2112e0bb8082b7836580a5
tamnguyen201 <tam2012000@gmail.com>
2020-08-25 14:24:48 +0700

    update code style

--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -15,18 +15,21 @@ class CompanyService
     public function index()
     {
         $Company = $this->CompanyRepository->first();
+
         return $Company;
     }
 
     public function count()
     {
         $Company = $this->CompanyRepository->count();
+
         return $Company;
     }
 
     public function create($results)
     {
         $data = $results;
+
         if(array_key_exists("logo", $results)) {
             $data['logo'] = $this->CompanyRepository->upload($results['logo']);
         }
@@ -39,12 +42,14 @@ class CompanyService
     public function show($id)
     {
         $Company = $this->CompanyRepository->show($id);
+
         return $Company;
     }
 
     public function update($results, $id)
     {
         $data = $results;
+
         if(array_key_exists("logo", $results)) {
             $data['logo'] = $this->CompanyRepository->upload($results['logo']);
         }
