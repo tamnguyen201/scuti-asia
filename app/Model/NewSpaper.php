004d055d2f3af9e6e575407b26fda91eddea2ca0
tamnguyen201 <tam2012000@gmail.com>
2020-08-29 21:59:59 +0700

    update

--- app/Model/NewSpaper.php
+++ app/Model/NewSpaper.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class NewSpaper extends Model
 {
     protected $table = 'new_spapers';
-    protected $fillable = ['title', 'image', 'description', 'url'];
+    protected $fillable = ['name', 'email', 'type', 'message'];
 }
