9004b2a66a85598ae3cc6a7bd6522e3e2431db41
tamnguyen201 <tam2012000@gmail.com>
2020-08-29 21:49:27 +0700

    update name table

--- app/Model/NewSpaper.php
+++ app/Model/NewSpaper.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class NewSpaperCompanies extends Model
+class NewSpaper extends Model
 {
     protected $table = 'new_spapers';
     protected $fillable = ['title', 'image', 'description', 'url'];
--- database/migrations/2020_08_05_010322_create_new_spaper_table.php
+++ database/migrations/2020_08_05_010322_create_new_spaper_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNewSpaperCompaniesTable extends Migration
+class CreateNewSpaperTable extends Migration
 {
     /**
      * Run the migrations.
