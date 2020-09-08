3a86c5ec9e6ae980a8a4b16686b8b74f1ec3a0c4
tamnguyen201 <tam2012000@gmail.com>
2020-09-08 09:59:55 +0700

    update

--- app/Repositories/Candidate/CandidateRepository.php
+++ app/Repositories/Candidate/CandidateRepository.php
@@ -8,22 +8,15 @@ class CandidateRepository extends Repository implements CandidateRepositoryInter
     public function getModel()
     {
         return \App\Model\User::class;
-<<<<<<< HEAD
     }
 
-    public function paginate($perPage = 15, $columns = array('*'))
+    public function paginate($perPage = 10, $columns = array('*'))
     {
         return $this->model->with(['userjob','job'])->latest()->paginate($perPage, $columns);
-=======
->>>>>>> 17201af5c97dc98f4909885c00099f4e0afdca24
     }
 
     public function show($id)
     {
-<<<<<<< HEAD
         return $this->model->with(['cv'])->findOrFail($id);
-=======
-        return $this->model->with('cv')->findOrFail($id);
->>>>>>> 17201af5c97dc98f4909885c00099f4e0afdca24
     }
 }
